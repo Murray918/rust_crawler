5b84333d2fd362d66ad8a5eb3dd99bb82e2bb1c0
Andrew Murray <darbyguitars@gmail.com>
2019-12-18 23:24:25 -0600

    implement TokenSink look for html a tags if found an open a tag get the href attrubute 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,48 @@ use html5ever::tokenizer::{
 use std::borrow::Borrow;
 
 use url::{
-    ParseError,
+    ParseError, 
     Url,
 };
 
 #[derive(Default, Debug)]
 struct LinkQueue {
-    links : Vec<String>,
+    links: Vec<String>,
+}
 
+impl TokenSink for &mut LinkQueue {
+    type Handle = ();
+
+    fn process_token(
+        &mut self,
+        token: Token,
+        line_number: u64
+    ) -> TokenSinkResult<Self::Handle> {
+        match token {
+            TagToken(
+                ref
+                tag
+                @
+                Tag {
+                    kind: TagKind::StartTag,
+                    ..
+                },
+            ) => {
+                if tag.name.as_ref() == "a" {
+                    for attribute in tag.attrs.iter() {
+                        if attribute.name.local.as_ref() == "href" {
+                            let url_str: &[u8] = attribute.value.borrow();
+                            self.links
+                                .push(String::from_utf8_lossy(url_str).into_owned());
+                        }
+                    }
+                }
+            }
+            _ => {}
+        }
+        TokenSinkResult::Continue
+    }
+}
 
 fn main() {
     println!("Hello, world!");
