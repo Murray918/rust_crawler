1c3a5a01e721d466941ce11126c002b12c9c2f23
Andrew Murray <darbyguitars@gmail.com>
2019-12-18 22:52:31 -0600

    added a imports for the webcrawler in Cargo.toml brouth in the need modules to main.rs  Initial commit 	new file:   .gitignore 	new file:   Cargo.lock 	new file:   Cargo.toml 	new file:   src/main.rs

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use html5ever::tokenizer::{
+    BufferQueue,
+    Tag,
+    TagKind,
+    TagToken,
+    Token,
+    TokenSink,
+    TokenSinkResult,
+    Tokenizer,
+    TokenizerOpts,
+};
+
+use std::borrow::Borrow;
+
+use url::{
+    ParseError,
+    Url,
+};
+
+
+fn main() {
+    println!("Hello, world!");
+}
