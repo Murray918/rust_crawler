8e016164fda174970858243fc9f032d472e9342f
Andrew Murray <darbyguitars@gmail.com>
2019-12-18 23:49:57 -0600

    implement function get_links get all the links that are relative and external return them in a a vec 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -56,6 +56,31 @@ impl TokenSink for &mut LinkQueue {
     }
 }
 
+pub fn get_links(url: &Url, page: String) -> Vec<Url> {
+    let mut domain_url = url.clone();
+    domain_url.set_path("");
+    domain_url.set_query(None);
+
+    let mut queue = LinkQueue::default(); // 0 out the links vector
+    //identify tokens in the html page
+    let mut tokenizer = Tokenizer::new(&mut queue, TokenizerOpts::default());
+    let mut buffer = BufferQueue::new(); //reading through the characters of our html page
+    buffer.push_back(page.into());
+    let _ = tokenizer.feed(&mut buffer);
+
+    queue
+        .links
+        .iter()
+        .map(|link| match Url::parse(link){
+            Err(ParseError::RelativeUrlWithoutBase) => domain_url.join(link).unwrap(),
+            Err(_) => panic!("Malformed Link found {}", link),
+            Ok(url) => url
+        }) 
+    .collect()
+
+}   
+
+
 fn main() {
     println!("Hello, world!");
 }
